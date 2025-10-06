df9aa8fafd8cd728137bc4c9d1bb4b3b8a350f3f
Hania <haniazipser2004@gmail.com>
2025-10-06 21:27:44 +0200

    Remove imports

--- src/main/java/uj/wmii/pwj/introduction/HelloWorld.java
+++ src/main/java/uj/wmii/pwj/introduction/HelloWorld.java
@@ -1,9 +1,5 @@
 package uj.wmii.pwj.introduction;
 
-import static uj.wmii.pwj.introduction.QuadraticEquation.findRoots;
-import static uj.wmii.pwj.introduction.Reverser.reverse;
-import static uj.wmii.pwj.introduction.Reverser.reverseWords;
-
 public class HelloWorld {
     public static void main(String[] args) {
 
